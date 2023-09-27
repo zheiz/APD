c91fd5b60560d826e109a434dd16a9675e9bc0be
Jeremiah Daligues Velasco <velascojeremiahd@gmail.com>
2023-09-27 11:50:57 +0800

    Added database dump

--- routes/web.php
+++ routes/web.php
@@ -29,8 +29,3 @@ Route::get('/registerpage', [RegisterAndLoginController::class, 'registerpage'])
 Route::get('/loginpage', function () {
     return view('main.loginpage');
 });
-
-Route::post('custom-login', [CustomAuthController::class, 'customLogin'])->name('login.custom'); 
-Route::get('registration', [CustomAuthController::class, 'registration'])->name('register-user');
-Route::post('custom-registration', [CustomAuthController::class, 'customRegistration'])->name('register.custom'); 
-Route::get('signout', [CustomAuthController::class, 'signOut'])->name('signout');
